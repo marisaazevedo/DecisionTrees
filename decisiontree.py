e3e0d15af938eee0fe5a18d042b937f3db9c51e7
Beatriz Sá <124071367+beatrizmsa@users.noreply.github.com>
2023-05-25 10:29:06 +0100

    Update decisiontree.py

--- decisiontree.py
+++ decisiontree.py
@@ -1,7 +1,7 @@
 import csv
 import argparse
 from sys import argv, exit
-##from tree import Tree
+from tree import Tree
 
 def main():
     parser = argparse.ArgumentParser()
@@ -13,21 +13,22 @@ def main():
     
     with open(args.filename,'rt') as file:
 
-        exemples_reader = csv.reader(file)
-        first_row = exemples_reader.__next__()
+        data_matrix_reader = csv.reader(file)
+        first_row = data_matrix_reader.__next__()
 
-        exemples = []
-        for line in exemples_reader:
-            exemples.append(line)
+        data_matrix = [] # matriz em que em cada posição está cada linha do csv
+        for line in data_matrix_reader:
+            data_matrix.append(line) 
 
-        attributes = {} ## vai ser um dicionario em que a chave é uma string
+        attributes = {}  # vai ser um dicionario em que cada chave é o nome da variável de cada coluna (string) 
+        # exemplo : {'ID': 0, 'sepallength': 1, 'sepalwidth': 2, 'petallength': 3, 'petalwidth': 4, 'class': 5}
         for position in range(len(first_row)):
             attributes[first_row[position]] = position
         
-        label_class = first_row[-1]
+        label_class = first_row[-1] 
         file.close()
 
-    ##tree = Tree(exemples,attributes,label_class)
-    print(attributes)
+    tree = Tree(data_matrix, attributes, label_class)
+    tree.entropia(data_matrix)
 
 main()
