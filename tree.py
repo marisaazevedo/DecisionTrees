f0efd9e12205528831252d6b88ce920c8e2566a2
marisaazevedo <marisapsazevedo@gmail.com>
2023-05-27 16:41:15 +0100

    add comments

--- tree.py
+++ tree.py
@@ -22,10 +22,10 @@ class Tree:
             self.reverse_attributes[position] = key
 
         # guarda os intervalos e o seu tipo do classe, para cada atributo do tipo inteiro
-        for index in range(1,len(attributes)):
+        for index in range(1, len(attributes)):
             try:
                 float(data_matrix[0][index])
-                self.set_intervalos(data_matrix, index)  
+                self.set_intervalos(data_matrix, index)
             except ValueError:
                 pass
         self.attribute_entropy(data_matrix)
@@ -42,7 +42,7 @@ class Tree:
 
         intervalo = []
         i = 0
-        
+
         for value, label in list_number:
             if i == 0:
                 min_value = value
@@ -56,51 +56,55 @@ class Tree:
                 else:
                     i = 0
             i += 1
-        
+
         intervalo.append((min_value, value + 0.1, label))
 
         self.transformations[self.reverse_attributes[index]] = intervalo
 
-    
+
     def entropy(self, data_label):
         count_classes = {}
         total = len(data_label)
         entropy = 0
-        
-        for valeu in data_label:
-            if  valeu not in count_classes:
-                count_classes[valeu] = 1
-            else:
-                count_classes[valeu] += 1
 
+        for value in data_label:
+            if  value not in count_classes:
+                count_classes[value] = 1
+            else:
+                count_classes[value] += 1
         for value in count_classes.values():
             entropy += -(value / total) * math.log2(value / total)
 
         return entropy
-    
-    def attribute_entropy(self,data_matrix):
-        
+
+    def attribute_entropy(self, data_matrix):
+
         for key, val in self.attributes.items():
+
+            # attributes = {'ID': 0, 'sepallength': 1, 'sepalwidth': 2, 'petallength': 3, 'petalwidth': 4, 'class': 5}
             self.entropy_atrributes[0] = 0
-            if key is not self.classe and key != 'ID':
-                unique_attributes = list(set(linha[val] for linha in data_matrix))
+            if key is not self.classe and key != 'ID': # verifica se o atributo não é a classe (ou seja, o atributo de destino) e não é a coluna 'ID'
+                unique_attributes = list(set(linha[val] for linha in data_matrix)) # armazenar os valores daquele atributo numa lista
                 entropy = 0
                 for value in unique_attributes:
                     subset = []
                     for i in range(len(data_matrix)):
                         if(data_matrix[i][val] == value):
                             subset.append(data_matrix[i][-1])
-                    subset_entropy = self.entropy(subset)
-                    subset_probability = len(subset)/ len(data_matrix)
-                    entropy += subset_probability * subset_entropy
-                self.entropy_atrributes[val] = entropy
+                    subset_entropy = self.entropy(subset) # entropia do subconjunto de classes correspondentes ao valor atual do atributo
+                    subset_probability = len(subset)/ len(data_matrix) # proporção de exemplos no subconjunto em relação ao conjunto de dados original
+                    entropy += subset_probability * subset_entropy # atualizar a entropia total do atributo
+                self.entropy_atrributes[val] = entropy # armazenar a entropia do atributo no dicionário
 
-        
+
     def information_gain(self):
-        info_gain = {}
+        info_gain = ''
+        best_value = 0
+        value = 0
         for key, val in self.attributes.items():
             if key is not self.classe and key != 'ID':
-                info_gain[key] = self.entropy_class - self.entropy_atrributes[val]       
-
+                value = self.entropy_class - self.entropy_atrributes[val]
+                if value > best_value:
+                    best_value = value
+                    info_gain = key
         return info_gain
-
